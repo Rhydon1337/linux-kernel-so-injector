531c918dd670bb1a64ee9bcd3718a7b5c0d31a4d
Rhydon <adirmak1@gmail.com>
2020-09-20 20:19:47 +0300

    tested using program which prints and sleeps

---
+++ test_program/sleeper.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>		/* open */
+#include <unistd.h>		/* exit */
+#include <sys/ioctl.h>		/* ioctl */
+
+int main() {
+	while (true){
+	std::cout << "here" << std::endl;
+	sleep(1);
+	}
+	return 0;
+}
